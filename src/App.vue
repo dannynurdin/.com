c17f2d3950465a99b9b8a66df8e74766c2c9bc84
dannynurdin <dannyfachrul@gmail.com>
2021-01-03 14:59:56 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="min-h-screen bg-baby-50">
+
+    <div class="md:mx-32">
+
+      <!-- Navigation -->
+      <div class=" flex justify-between items-center bg-bluegreen text-center text-bluenavy text-xl h-12 px-2 rounded-md shaddow-lg">
+        <div class="">Logo</div>
+        <div class="">Menu</div>
+        <div class="">Button</div>
+      </div>
     </div>
-    <router-view/>
+    
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        bluegreen: '#75E6DA',
+        bluegrotto: '#189AB4',
+        bluenavy: '#05445E',
+        baby: {
+          pure: '#D4F1F4',
+          50: '#EFFAFB',
+          100: '#def5f7',
+          200: '#bdebef',
+          300: '#9ce1e7',
+          400: '#7cd7df',
+          500: '#5bcdd7',
+          600: '#3ac3cf',
+          700: '#2ca9b5',
+          800: '#248a94',
+          900: '#1c6c73'
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
